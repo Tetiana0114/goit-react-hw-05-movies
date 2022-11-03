e852db0e73ba63946e1792bb019c517c390aa159
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-11-03 22:25:05 +0200

    New

---
+++ src/services/fetchMovies.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const KEY = '93fca4687d78764c2a220d92f59e9e96';
+const BASIC_URL = 'https://api.themoviedb.org/3/';
+
+export const getTrendingMovie = async () => {
+    const response = await axios.get(`${BASIC_URL}trending/movie/day?api_key=<<${KEY}>>`);
+    return response.data.results;
+};
+
+export const searchMovies = async ({ query }) => {
+    const response = await axios.get(`${BASIC_URL}search/movie?api_key=<<${KEY}>>&language=en-US&query=${query}&page=1&include_adult=false`);
+    return response.data.results;
+};
+  
+export const getMovieDetails = async ({ movie_id }) => {
+    const response = await axios.get(`${BASIC_URL}movie/${movie_id}?api_key=<<${KEY}>>&language=en-US`);
+    return response.data;
+};
+
+export const getMovieCredits = async ({ movie_id }) => {
+    const response = await axios.get(`${BASIC_URL}movie/${movie_id}/credits?api_key=<<${KEY}>>&language=en-US`);
+    return response.data.cast;
+};
+
+export const getMovieReviews = async ({ movie_id }) => {
+    const response = await axios.get(`${BASIC_URL}movie/${movie_id}/reviews?api_key=<<${KEY}>>&language=en-US&page=1`);
+    return response.data.results;
+};
