df6e722c9207d2e8d052f3756a7f0e5bd6fc5b5e
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-11-04 22:20:25 +0200

    New

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,25 @@
-import SearchBar from 'components/SearchBar';
+// import { Route, Routes } from "react-router-dom";
+// import { Home } from '../pages/Home/Home';
+import { IoMdFilm } from "react-icons/io";
+import { Container, Header, Link, Logo } from "./App.styled";
+
 
 
 export const App = () => {
   return (
-    <div
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <SearchBar/>
-    </div>
+    <Container>
+      <Header>
+        <Logo>
+          <IoMdFilm size={40}/>
+        </Logo>
+          <nav>
+          <Link to="/" end>Home</Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </Header>
+        {/* <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes> */}
+    </Container>
   );
 };
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+// import PropTypes from 'prop-types';
+// import getTrendingMovie from '../services/fetchMovies';
+
+// export const Home = () => {
+//   return (
+//     <main>
+//       <h1>In trend:</h1>
+  
+//     </main>
+//   );
+// };
+
